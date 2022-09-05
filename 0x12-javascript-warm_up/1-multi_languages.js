e3e84d0d113868634fdbacc37c9c678d22a0c053
masilanzuki <massilaeugene@gmail.com>
2022-09-05 20:46:13 +0300

    js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let firstMessage = 'C is fun'
-let secondMessage = 'Python is cool'
-let thirdMessage = 'JavaScript is amazing'
+const firstMessage = 'C is fun';
+const secondMessage = 'Python is cool';
+const thirdMessage = 'JavaScript is amazing';
 
 console.log(firstMessage);
 console.log(secondMessage);
