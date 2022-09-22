f86477996a5026dcecafbf27211dd6e3b64eb494
masilanzuki <massilaeugene@gmail.com>
2022-09-22 13:24:11 +0300

    3rd

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # curl takes URL sends GET request and displays the body of the response
 #displays header x-school-user-id with a value 98
-curl -sH "X-School-User-Id:98" "$1"
+curl -sH "X-School-User-Id: 98" "$1"
